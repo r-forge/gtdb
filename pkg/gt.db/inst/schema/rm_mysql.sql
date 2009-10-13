03393f00a9215a991526b7aa3c0a51fcf912fc3f
dahinds (David Hinds) <dhinds@sonic.net>
2009-10-13 22:01:36 +0000

    Added scripts for deleting schema objects
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gtdb@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/gt.db/inst/schema/rm_mysql.sql
@@ -0,0 +1,40 @@
+--
+-- Copyright (C) 2009, Perlegen Sciences, Inc.
+-- 
+-- Written by David A. Hinds <dhinds@sonic.net>
+-- 
+-- This is free software; you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the license, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>
+-- 
+
+drop table prcomp_loading;
+drop table prcomp_component;
+drop table prcomp;
+drop table test_result;
+drop table test;
+drop table assay_data;
+drop table assay_data_flag;
+drop table sample_value;
+drop table sample_attr;
+drop table sample;
+drop table subject_value;
+drop table subject_attr;
+drop table subject;
+drop table dataset;
+drop table project;
+drop table assay_position;
+drop table mapping;
+drop table assay;
+drop table assay_flag;
+drop table assay_group;
+drop table platform;
