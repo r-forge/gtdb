04e8b4cea0c8cc4524b605666179c17ca21405fe
dahinds (David Hinds) <dhinds@sonic.net>
2009-10-30 19:43:05 +0000

    Updated description, added "getting started" section
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gtdb@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -33,19 +33,55 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<h2>Welcome to the Genotype Database Framework, GT.DB</h2>
+
+<style type="text/css">p.big {line-height: 1.5}</style>
+<p class="big">
+GT.DB is a database framework for efficiently managing genome-wide
+genotype datasets (i.e. 10<sup>6</sup> or more biallelic markers, 1000s of
+samples), as well as underlying raw data and complex phenotype data,
+tightly integrated with R for analysis.  It is based on components
+developed at Perlegen Sciences, where a similar framework was used to
+manage nearly 10<sup>11</sup> individual genotypes with underlying data,
+representing more than 100,000 samples and more than 100 experimental
+datasets.
+</p>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> The <strong>project summary page</strong> can be found <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<h3>Getting Started</h3>
+
+<ul>
+  <li>Install the gt.db package
+<pre>
+install.packages("gt.db", repos="http://R-Forge.R-project.org")
+library(gt.db)
+</pre>
+  </li>
+  <li>Create a temporary SQLite database
+<pre>
+library(SQLite)
+fname <- tempfile()
+dbh <- dbConnect(dbDriver('SQLite'), fname)
+</pre>
+  </li>
+  <li>Create the gt.db tables, and load up the demo datasets
+<pre>
+use.gt.db(dbh)
+init.gt.db()
+.gt.db.options(db.mode='hex')
+demo('setup.gt.demo')
+</pre>
+  </li>
+  <li>Explore the code examples!
+<pre>
+example(prcomp.gt.data)
+example(xyplot.gt.data)
+</pre>
+  </li>
+</ul>
 
 </body>
 </html>
